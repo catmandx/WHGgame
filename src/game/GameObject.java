3874a5f704de6d73cd03d35adff3711f129c50ba
catmandx <hhoang.nov.13@gmail.com>
2019-06-15 22:32:19 +0700

    Limit movement & Line, VLine, HLine added

--- src/game/GameObject.java
+++ src/game/GameObject.java
@@ -8,33 +8,39 @@ import java.util.ArrayList;
 public class GameObject {
     //quan li doi tuong
     public static ArrayList<GameObject> objects = new ArrayList<>();
+    public Vector2D position;
+    public Vector2D velocity;
+    public BoxCollider hitBox;
+    public boolean active;
+    public Vector2D anchor; //vi tri tam anh
 
     public GameObject() {
         objects.add(this);
         position = new Vector2D();
         velocity = new Vector2D();
-        anchor = new Vector2D(0.5,0.5);
+        anchor = new Vector2D(0.5, 0.5);
     }
 
-    public static <E extends GameObject> E findInactive(Class<E> cls){
+    public static <E extends GameObject> E findInactive(Class<E> cls) {
         for (int i = 0; i < objects.size(); i++) {
             GameObject object = objects.get(i);
-            if(cls.isAssignableFrom(object.getClass())
-                    && !object.active){
+            if (cls.isAssignableFrom(object.getClass())
+                    && !object.active) {
                 return (E) object;
             }
         }
         return null;
     }
-    public static <E extends GameObject> E findIntersects(Class<E> cls, BoxCollider hitBox){
+
+    public static <E extends GameObject> E findIntersects(Class<E> cls, BoxCollider hitBox) {
         //E ~Enemy |Player|bullet....
         //cls ~ Enemy.class | Player.class | Enemy.class..
         for (int i = 0; i < objects.size(); i++) {
             GameObject object = objects.get(i);
-            if(object.active
+            if (object.active
                     && object.hitBox != null
                     && object.hitBox.intersects(hitBox)
-                    && cls.isAssignableFrom(object.getClass())){
+                    && cls.isAssignableFrom(object.getClass())) {
                 return (E) object;
             }
         }
@@ -47,30 +53,24 @@ public class GameObject {
         }
     }
 
-    public static void runAll(){
+    public static void runAll() {
         for (int i = 0; i < objects.size(); i++) {
             objects.get(i).run();
         }
     }
 
-    public Vector2D position;
-    public Vector2D velocity;
-    public BoxCollider hitBox;
-    public boolean active;
-    public Vector2D anchor; //vi tri tam anh
-
     public void paint(Graphics g) {
         //debug
         g.setColor(Color.CYAN);
-        g.drawRect((int)(hitBox.left()),
-                (int)(hitBox.top()),
+        g.drawRect((int) (hitBox.left()),
+                (int) (hitBox.top()),
                 hitBox.width,
                 hitBox.height);
         g.setColor(Color.YELLOW);
-        g.fillOval((int) (position.x-3), (int)(position.y-3), 5,5);
+        g.fillOval((int) (position.x - 3), (int) (position.y - 3), 5, 5);
     }
 
-    public void run(){
+    public void run() {
         position.add(velocity);
     }
 }
